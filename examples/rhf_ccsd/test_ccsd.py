0577bf1d89c52ed73858e9d199d8c34de67fd50d
Karthik Gururangan <gururang@msu.edu>
2024-09-12 12:37:59 -0400

    Added tests for the interface between UEG-CCD and Miniccpy.

---
+++ examples/rhf_ccsd/test_ccsd.py
@@ -0,0 +1,72 @@
+import numpy as np
+from miniccpy.printing import print_custom_system_information, print_custom_system_information_rhf
+from miniccpy.driver import run_cc_calc, run_mpn_calc
+from miniccpy.integrals import spatial_to_spinorb
+
+def init_heg():
+    with open("nocc", "r") as f:
+        nocc = int(f.readlines()[0])
+    with open("nelectrons", "r") as f:
+        nelectron = int(f.readlines()[0])
+    with open("nbasis", "r") as f:
+        norb = int(f.readlines()[0])
+    return nelectron, norb, nocc
+
+def read_fock(nbasis):
+    '''Reads the Fock matrix in onebody.inp.'''
+    fock = np.zeros((nbasis, nbasis))
+    with open("onebody.inp", "r") as f:
+        lines = f.readlines()
+        cnt = 0
+        for p in range(nbasis):
+            for q in range(p + 1):
+                fock[p, q] = float(lines[cnt].split()[0])
+                fock[q, p] = fock[p, q]
+                cnt += 1
+    return fock 
+
+def read_twobody(nbasis):
+    '''Reads the ERI matrix (in physics notation) in twobody.inp.'''
+    eri = np.zeros((nbasis, nbasis, nbasis, nbasis))
+    with open("twobody.inp", "r") as f:
+        for line in f:
+            L = line.split()
+            p, q, r, s = [int(x) - 1 for x in L[:-1]]
+            val = float(L[-1])
+            # nuclear repulsion value
+            if p == -1 and q == -1 and r == -1 and s == -1:
+                nuclear_repulsion = val
+            else:
+                eri[p, q, r, s] = val
+    return eri, nuclear_repulsion
+
+def test_ccsd():
+
+    nelectrons, nbasis, nocc = init_heg()
+    nfrozen = 0
+    hf_energy = 0.0
+
+    o = slice(0, nocc)
+    v = slice(nocc, nbasis)
+
+    print("   Reading Fock matrix")
+    fock = read_fock(nbasis)
+
+    print("   Reading ERI matrix")
+    g, _ = read_twobody(nbasis)
+    print("")
+
+    print_custom_system_information_rhf(fock, nelectrons, nfrozen, hf_energy)
+
+    T, E_corr = run_cc_calc(fock, g, o, v, method='rccsd', maxit=80)
+
+    #
+    # Check the results
+    #
+    assert np.allclose(E_corr, -0.2979101676, atol=1.0e-08) 
+
+if __name__ == "__main__":
+    test_ccsd()
+
+
+
---
+++ source/Exporter.f90
@@ -0,0 +1,105 @@
+Module Exporter
+
+Use Precision
+Use Constants
+Use Pointers
+
+Implicit None
+
+private
+public :: ExportUEGIntegrals
+
+Contains
+
+        Subroutine ExportUEGIntegrals(HEGData,UEGInfo)
+              
+              Use Types, only: HEGDataType, UEGInfoType
+              Use HEG, only: FindIndex
+              
+              Type (HEGDataType), Intent(In) :: HEGData
+              Type (UEGInfoType), Intent(In) :: UEGInfo
+
+              Integer, Parameter :: io=101
+              Integer :: p, q, r, s, cnt, a, b, i, j
+              Real (Kind=pr) :: eri_val, eri_val2, fock_val
+
+              write(6,*) 'Printing UEG Info Parameters'
+              write(6,*) '---------------------------'
+              write(6,*) 'Number of Electrons:', UEGInfo%Nelectron
+              write(6,*) 'Number of Occupied:', UEGInfo%NOcc
+              write(6,*) 'Number of basis functions:', UEGInfo%NAO
+
+              open(unit=io, file='nelectrons', status='replace')
+              write(io,*) UEGInfo%Nelectron
+              close(io)
+
+              open(unit=io, file='nbasis', status='replace')
+              write(io,*) UEGInfo%NAO
+              close(io)
+
+              open(unit=io, file='nocc', status='replace')
+              write(io,*) UEGInfo%NOcc
+              close(io)
+
+              open(unit=io, file='onebody.inp', status='replace')
+              write(6,*) 'Writing onebody.inp'
+              cnt = 1
+              do p = 1,UEGInfo%NAO
+                 do q = 1,p
+                    if (p == q) then
+                       fock_val = HEGData%Eigen(p)
+                    else
+                       fock_val = 0.0_pr
+                    end if
+                    write(io,*) fock_val, cnt
+                    cnt = cnt + 1
+                 end do
+              end do
+              close(io)
+              open(unit=io, file='twobody.inp', status='replace')
+              write(6,*) 'Writing twobody.inp'
+              ! Write only permutationally unique integrals
+              !do p = 1,nbf
+              !   do r = 1,p
+              !      pr = p*(p + 1)/2 + r
+              !      do q = 1,nbf
+              !         do s = 1,q
+              !            qs = q*(q + 1)/2 + s
+              !            if (pr > kl) then
+              !                write(io,*) p, q, r, s, eri_val
+              !            end if
+              !         end do
+              !      end do
+              !   end do
+              !end do
+              ! Write all integrals straight
+              do p=1,UEGInfo%NAO
+                 do q=1,UEGInfo%NAO
+                    do r=1,UEGInfo%NAO
+                       ! Use the momentum-allowed indexing
+                       s = FindIndex(HEGData,UEGINfo%MaxKPoint,UEGInfo%NAO,p,q,r)
+                       if (s<=0) cycle
+                       eri_val = ERI(HEGData,UEGInfo,p,q,r,s,DummyFlag=0)
+                       if (eri_val < -80_pr) cycle
+                       write(io,*) p, q, r, s, eri_val
+                    end do
+                 end do
+              end do
+              ! write nuclear repulsion (set to 0 for now)
+              write(io,*) 0, 0, 0, 0, 0.0_pr
+              close(io)
+              !open(unit=io, file='mask', status='replace')
+              !write(6,*) 'Writing T2 mask'
+              !do a=nocc+1,nbf
+              !   do i=1,nocc
+              !      do j=1,nocc
+              !         b = FindIndex(i,j,a)
+              !         if (b <= nocc) cycle
+              !         write(io,*) a - nocc, b - nocc, i, j
+              !      end do
+              !   end do
+              !end do
+              !close(io)
+      End Subroutine ExportUEGIntegrals
+
+End Module Exporter
